606d08367382b92e9dcb6e6ec7f82df9e35b63b2
Clemente <clementesc@gmail.com>
2021-08-14 12:31:31 -0300

    Improved person creation funcionality

--- src/main/java/com/dio/santander/peopleapi/controller/PersonController.java
+++ src/main/java/com/dio/santander/peopleapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.dio.santander.peopleapi.controller;
 
+import com.dio.santander.peopleapi.dto.request.PersonDTO;
 import com.dio.santander.peopleapi.dto.response.MessageResponseDTO;
 import com.dio.santander.peopleapi.entity.Person;
 import com.dio.santander.peopleapi.service.PersonService;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/person")
@@ -24,8 +27,8 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 
 }
